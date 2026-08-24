d129d8ea4e697d625d43860d814f4f9e22a81def
TheStrongWallGambit <arya.chellappah@student.rjuhsd.us>
2026-08-24 13:13:16 -0700

    Update stringIntro.java

--- stringIntro.java
+++ stringIntro.java
@@ -2,7 +2,28 @@ import javax.sound.midi.SysexMessage;
 import java.util.*;
 public class stringIntro{
     public static void main(String[] args){
+
+        int num =5;
+        double weight =7.89;
+
+        Integer number = new Integer(5);//unboxing
+        Integer number2 =5; // auto unboxing
+
+        Double w = new Double(7.89); // boxing
+        Double W = 7.89;// auto boxing
+
+        System.out.println(number2 == num); //auto-unboxing
+        System.out.println(w == weight); //auto-unboxing
+
+        double newNum = w; //auto-unboxing
+        int age = number2; // auto-unboxing
+
+        int year = number2.intValue(); //unboxing
+        int average = W.doubleValue(); //unboxing
+
+        System.out.println(number2.MIN_VALUE);
         String address = new String("I Grizzly Way, Granite Bay, CA");//AP Test Way
+
 
         String location= "Yosemite National Park";
 
@@ -33,5 +54,24 @@ public class stringIntro{
 
         String[] listOfLetters = location.split("a");
         System.out.println(Arrays.toString(listOfLetters));
+
+        char letter = location.charAt(9);
+        System.out.println(letter);
+        boolean a = address.contains("Grizz"); // doesn't care about literal or object
+        System.out.println(a);
+
+        String g = "in";
+        System.out.println(g.isEmpty());
+        location = location.replace("a","*");
+        System.out.println(location);
+        location = location.toLowerCase();
+        System.out.println(location);
+        location = location.toUpperCase();
+        System.out.println(location);
+        location = location.trim();
+        System.out.println(location);
+
+
+    
     }
 }
